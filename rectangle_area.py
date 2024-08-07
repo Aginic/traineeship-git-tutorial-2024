6d64828f56148ae6dc0c340c7771ed5fa80b0148
MarkQiu-CD <mark.qiu@countdown.co.nz>
2024-08-08 11:13:11 +1200

    Update rectangle calculator

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,7 +1,14 @@
+
 def get_area_of_rectangle( side_a, side_b):
+    return 2 * (side_a + side_b)
+
+def get_perimeter_of_rectangle( side_a, side_b):
     return 2 * (side_a + side_b)
 
 if __name__ == "__main__":
     side_a = 5
     side_b = 8
-    print(f"The area of a {side_a} by {side_b} rectangle is {get_area_of_rectangle(side_a, side_b)}!")
+    area = get_area_of_rectangle(side_a, side_b)
+    perimeter = get_area_of_rectangle(side_a, side_b)
+    print(f"The area of a {side_a} by {side_b} rectangle is {area}!")
+    print(f"The perimeter of a {side_a} by {side_b} rectangle is {perimeter}!")
