06f401f2d69506213c2c08b0e8006cb32382eb4b
QuynhLe2596 <quynh.le@mantelgroup.com.au>
2024-08-12 14:31:11 +1000

    fix perimeter calculation (#10)
    Co-authored-by: Quynhle2596 <quynh.le@mantelgrouo.com.au>

--- rectangle_calculator.py
+++ rectangle_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     side_a = 6
     side_b = 8
     area = get_area_of_rectangle(side_a, side_b)
-    perimeter = get_area_of_rectangle(side_a, side_b)
+    perimeter = get_perimeter_of_rectangle(side_a, side_b)
     print(f"The area of a {side_a} by {side_b} rectangle is {area}!")
     print(f"The perimeter of a {side_a} by {side_b} rectangle is {perimeter}!")
 
